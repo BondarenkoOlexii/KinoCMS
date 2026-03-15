3688dd72a19953ff4726bc2dee33e8f6739bb3f6
administrator <bondarenkoalosg@gmail.com>
2026-03-15 18:05:58 +0100

    return signals

--- src/adminpanel/apps.py
+++ src/adminpanel/apps.py
@@ -5,3 +5,5 @@ class AdminpanelConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'src.adminpanel'
 
+    def ready(self):
+        import src.adminpanel.signals
